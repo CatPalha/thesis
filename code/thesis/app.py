73bad7f5bcb08f75a246e892cb93e35646b4a8e2
CatPalha <ferrcat@gmail.com>
2021-03-27 01:38:44 +0000

    Fixed: pause functionality on simulation, it is working. TODO: back key to return to the main menu Started the user inputs on parameters

--- code/thesis/app.py
+++ code/thesis/app.py
@@ -12,8 +12,8 @@ class App():
     def __init__(self):
 
         pygame.init()
-        self.run, self.loop, self.playing = True, False, False
-        self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
+        self.run, self.pause, self.playing = True, True, False
+        self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY, self.SPACE_KEY = False, False, False, False, False
         self.width, self.height, self.fps = 1000, 800, 60
         self.white, self.gray, self.black = (255, 255, 255), (200,200,200), (0, 0, 0)
         #self.font_name = 'ARIAL'
@@ -21,7 +21,9 @@ class App():
 
         self.display = pygame.Surface(((self.width, self.height)))
         self.win = pygame.display.set_mode((self.width, self.height))
+
         self.clock = pygame.time.Clock()
+        self.fps = 60
 
         self.main_menu = MainMenu(self)
         self.parameters = ParametersMenu(self)
@@ -50,7 +52,8 @@ class App():
             if event.type == pygame.KEYDOWN:
 
                 if event.key == pygame.K_SPACE:
-                    self.loop = False
+                    self.SPACE_KEY = True
+                    self.pause_restart()
 
                 if event.key == pygame.K_RETURN:
                     self.START_KEY = True
@@ -64,30 +67,70 @@ class App():
                 if event.key == pygame.K_UP:
                     self.UP_KEY = True
             
+    
+    def check_events_pause(self):
+
+        self.pause = True
+        #pygame.mixer.pause()
+        
+        while self.pause:
+
+            for event in pygame.event.get():
+
+                if event.type == pygame.QUIT: 
+                    pygame.quit()
+                    quit()
+
+                if event.type == pygame.KEYDOWN:
+
+                    if event.key == pygame.K_SPACE:
+        
+                        self.pause = False
+                    
+                    if event.key == pygame.K_BACKSPACE:
+                        #TODO
+                        pass
+                        
+    
+                    
+
+
 
     def reset_keys(self):
 
-        self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
+        self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY, self.SPACE_K = False, False, False, False, False
 
 
     def draw_text(self, text: str, size: int, x_text_pos: float, y_text_pos: float):
 
         font = pygame.font.Font(self.font_name, size)
         text_surface = font.render(text, True, self.black)
+
         text_rect = text_surface.get_rect()
         text_rect.center = (x_text_pos, y_text_pos)
         self.display.blit(text_surface, text_rect)
 
+    def pause_restart(self):
+
+        self.check_events_pause()
+
+
 
     def main_loop(self):
         #Inside start option, the simulation itself
-        
+        self.clock.tick(self.fps)
+
         while self.playing:
+
             self.check_events()
 
-            if self.START_KEY:
+            if self.BACK_KEY:
                 self.playing = False
 
+            # if self.SPACE_KEY:
+            #     self.pause_restart()
+
+           
             self.win.fill(self.white)
             pygame.display.set_caption("Midge Ecosystem")
 
@@ -129,41 +172,5 @@ for i in range(3):
 
 
 
-    # def to_run(self):
-
-    #     while self.run:
-            
-    #         self.clock.tick(self.fps)
-    #         #self.check_events()
-    #         self.main_loop()
-    #         self.win.fill(self.white)
-    #         pygame.display.set_caption("Midge Ecosystem")
-
-    #         self.draw_agent("cocoa_tree.png", 133, 135, 300, 200)
-    #         self.draw_agent("cocoa_tree.png", 133, 135, 500, 200)
-    #         self.draw_agent("water.png", 100, 80, 425, 300)
-
-    #         for midge in midges:
-
-    #             midge.random_movement()
-    #             midge.borders()
-    #             self.draw_agent("midge.png", 350, 150, midge.x, midge.y)
-
-    #         for mite in mites:
-
-    #             mite.random_movement()
-    #             mite.borders()
-    #             self.draw_agent("mite.png", 400, 200, mite.x, mite.y)
-
-            
-    #         pygame.display.update()
-    #         self.reset_keys()
-
-    # print("How many midges?")
-
-
-
-# print("How many trees?")
-
-# midge_number = int(input())
+  
 
--- code/thesis/menu.py
+++ code/thesis/menu.py
@@ -120,13 +120,16 @@ class ParametersMenu(Menu):
     def __init__(self, app):
         Menu.__init__(self, app)
 
-        self.state = "Ecosystem"
+ 
         self.ecosystem_x, self.ecosystem_y = self.mid_width, self.mid_height - 80
         self.agents_x, self.agents_y = self.mid_width, self.mid_height - 20
         #self.reproduction_x, self.reproduction_y = self.mid_width, self.mid_height + 40
         self.agent_input_x, self.agent_input_y = self.mid_width, self.mid_height - 100
-        # self.wordBox_1 = makeTextBox(self.mid_width, self.mid_height, "Enter number of agents here", 0, 40)
-        # self.entry_1 = textBoxInput(self.wordBox_1)
+
+        # self.input_font = pygame.font.Font(self.app.font_name, 32)
+        # self.user_input = "TEST"
+        # self.input_surface = self.input_font.render(self.user_input, True, self.app.black)
+        # self.input_pos = (0, 10)
 
 
     def display_menu(self):
@@ -136,55 +139,39 @@ class ParametersMenu(Menu):
 
             self.app.check_events()
             self.check_input()
-            self.app.display.fill(self.app.gray)
-        
-            #self.app.draw_text("Parameters", 60, self.app.width / 2, 100)
-            # self.app.draw_text("Ecosystem", 50, self.ecosystem_x, self.ecosystem_y)
-            # self.app.draw_text("Agents", 50, self.agents_x, self.agents_y)
-            #self.app.draw_text("Reproduction", 50, self.reproduction_x, self.reproduction_y)
+            
 
             #self.draw_cursor()
             self.app.display.fill(self.app.gray)
+
+            #TODO: parameters user input 
+            
+            # self.app.win.blit(self.input_surface, self.input_pos)
+            # pygame.display.flip()
+            self.app.draw_text("", 20, 30, 10)
             self.app.draw_text("Insert the parameters", 60, self.app.width / 2, 100)
             self.app.draw_text("Number of Midges:", 40, self.agent_input_x - 90, self.agent_input_y - 30)
             self.app.draw_text("Number of Pretators:", 40, self.agent_input_x - 70, self.agent_input_y + 30)
             self.app.draw_text("Reproduction probability:" , 40, self.agent_input_x - 18, self.agent_input_y + 90)
-            
 
             self.window()
 
     def check_input(self):
 
         if self.app.BACK_KEY:
+
             self.app.current_menu = self.app.main_menu
             self.menu_running = False
 
-        # elif self.app.UP_KEY or self.app.DOWN_KEY:
+        if self.app.SPACE_KEY:
+            self.app.check_events_pause()
 
-        #     if self.state == "Ecosystem":
-        #         self.state = "Agents"
-        #         self.cursor_rect.midtop = (self.agents_x + self.offset, self.agents_y)
-            
-        #     elif self.state == "Agents":
-        #         self.state = "Ecosystem"
-        #         self.cursor_rect.midtop = (self.ecosystem_x + self.offset, self.ecosystem_y)
+    
 
-        # elif self.app.START_KEY:
-            
-        #     if self.state == "Agents":
-
-        #         self.app.display.fill(self.app.gray)
-        #         self.app.draw_text("Choose your parameters", 60, self.app.width / 2, 100)
-        #         self.app.draw_text("Number of Agents: ", 50, self.agent_input_x, self.agent_input_y)
-
-        #         self.window()
-            
-        #     else:
-        #         pass
+        
 
                 
 
-    
 
     
 class RulesMenu(Menu):
